ccdb5e8d899d463eb16472613cddfa4ccbd7378e
Chung-kai-yu <m11112089@yuntech.edu.tw>
2023-10-09 04:09:18 +0800

    day22'23

---
+++ Day23/MathFunctions/include/mysqrt.h
@@ -0,0 +1,4 @@
+#ifndef MYSQRT_H
+#define MYSQRT_H
+double sqrt(double x);
+#endif // MYSQRT_H
---
+++ Day23/MathFunctions/src/mysqrt.cpp
@@ -0,0 +1,19 @@
+#include "mysqrt.h"
+#include <iostream>
+double sqrt(double x)
+{
+  if (x <= 0) return 0;
+  double result = x;
+
+  for (int i = 0; i < 5; ++i) {
+    if (result <= 0) {
+      result = 0.1;
+    }
+    double delta = x - (result * result);
+    result = result + 0.5 * delta / result;
+    std::cout << i << "Computing sqrt of " << x << " to be " << result << std::endl;
+  }
+  return result;
+}
+
+
